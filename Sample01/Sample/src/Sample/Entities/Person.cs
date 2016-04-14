3eb6c3b458484909b54171a0ab5c0698a0df717c
debugair <stefan.kaspar@live.com>
2016-04-14 23:16:29 +0200

    Changes

--- Sample01/Sample/src/Sample/Entities/Person.cs
+++ Sample01/Sample/src/Sample/Entities/Person.cs
@@ -5,6 +5,5 @@
         public int? Id { get; set; }
         public string Familyname { get; set; }
         public string Firstname { get; set; }
-        public string Middlename { get; set; }
     }
 }
