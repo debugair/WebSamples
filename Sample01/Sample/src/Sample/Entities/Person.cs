b2333bfceb4efb0bc4390d25850d31721e99f714
debugair <stefan.kaspar@live.com>
2016-04-14 22:01:38 +0200

    Solution Task 01 Project

---
+++ Sample01/Sample/src/Sample/Entities/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace Sample.Entities
+{
+    public class Person
+    {
+        public int? Id { get; set; }
+        public string Familyname { get; set; }
+        public string Firstname { get; set; }
+        public string Middlename { get; set; }
+    }
+}
---
+++ Sample01/Sample/src/Sample/Repositories/PersonRepository.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Sample.Entities;
+
+namespace Sample.Repositories
+{
+    public static class PersonRepository
+    {
+        public static IList<Person> Persons { get; set; }
+
+        static PersonRepository()
+        {
+            Persons = new List<Person>();
+        }
+
+        public static IEnumerable<Person> ReadAll()
+        {
+            return Persons;
+        }
+
+        public static Person Read(int? id)
+        {
+            return Persons.FirstOrDefault(p => p.Id == id);
+        }
+
+        public static Person Add(Person person)
+        {
+            person.Id = (Persons.Count > 0) ? Persons.Max(p => p.Id) + 1 : 1;
+            Persons.Add(person);
+            return person;
+        }
+
+        public static Person Change(Person person)
+        {
+            if (Persons.All(p => p.Id != person.Id)) return null;
+
+            for (int pos = 0; pos < Persons.Count; pos++)
+            {
+                if (Persons[pos].Id != person.Id) continue;
+                Persons.RemoveAt(pos);
+                Persons.Add(person);
+                break;
+            }
+            return person;
+        }
+
+        public static void Remove(int id)
+        {
+            if (Persons.All(p => p.Id != id)) return;
+
+            for (int pos = 0; pos < Persons.Count; pos++)
+            {
+                if (Persons[pos].Id != id) continue;
+                Persons.RemoveAt(pos);
+                break;
+            }
+        }
+    }
+}
